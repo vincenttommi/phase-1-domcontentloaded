7bb103b2e92d83935e8b24b35e90c1b3b80d9bec
vincenttommi <vincenttommi@gmail.com>
2023-03-24 14:16:07 +0300

    changes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
-
-console.log("The DOM has loaded");
-
+    
+    document.querySelector("#text").innerHTML ="JavaScript is so cool. It lets me add text to my page programmatically.";
 
 
 });
 
-console.log("This  console.log() fires when index.js loads - before DOMContentLoaded is triggered");
